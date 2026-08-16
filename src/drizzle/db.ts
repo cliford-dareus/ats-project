af90c2ec271d868a50c1dd0e6258f23c6b078955
Cliford M. Dareus <104238549+cliford-dareus@users.noreply.github.com>
2026-08-16 14:36:04 -0400

    Use DATABASE_URL for database connection

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -13,10 +13,10 @@ declare global {
 function createDatabaseConnection() {
     const pool = mysql.createPool({
         uri: process.env.DATABASE_URL,
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
+        // host: process.env.DB_HOST,
+        // user: process.env.DB_USER,
+        // password: process.env.DB_PASSWORD,
+        // database: process.env.DB_NAME,
         waitForConnections: true,
         connectionLimit: 10,
         queueLimit: 0,
